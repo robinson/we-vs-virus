40254f039b4440db467deac93392f005bb298826
Marlen Kheder <kheder@me.com>
2020-03-21 12:58:50 +0100

    Added react native mobile project

---
+++ Mobile/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Wir kämpfen </Text>
+      <Text style={styles.text}> gegen CORONA </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text:{
+    fontSize:30,
+    color:'white',
+    alignItems:'center',
+    justifyContent:"center" 
+  }
+});
---
+++ Mobile/README.md
@@ -0,0 +1,3 @@
+## C19 Test by Ambulance
+
+This App is developed to help people in the situation where C19 is spreading fast..
