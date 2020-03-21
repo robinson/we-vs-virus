572ec96f00ff30c8243ac470dff22bbc9d0208c3
Marlen Kheder <kheder@me.com>
2020-03-21 23:53:36 +0100

    sendgrid email ids

--- Business/Utility/EmailTemplateIdsConfiguration.cs
+++ Business/Utility/EmailTemplateIdsConfiguration.cs
@@ -3,5 +3,10 @@
     public class EmailTemplateIdsConfiguration
     {
         public string DriverSignUpConfirmationLink { get; set; }
+        public string HealthOfficeSignUpConfirmationLink { get; set; }
+        public string UserSignUpConfirmationLink { get; set; }
+        public string SwabPickedUp { get; set; }
+        public string SwabDelivered { get; set; }
+        public string OrderCanceled { get; set; }
     }
 }
