580c5f488507ee2ab4b735140a7227b8b9aefc35
Hidayet Gencer <hidayet@gmail.com>
2020-03-21 15:01:01 +0100

    Test push

--- Mobile/App.js
+++ Mobile/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Wir kämpfen </Text>
-      <Text style={styles.text}> gegen CORONA </Text>
+      <Text style={styles.text}> gegen CORONA! </Text>
     </View>
   );
 }
